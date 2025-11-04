db7dacc6cf087fa5c46bff6347628394031bed45
Nicolas <ni-vanico@hotmail.com>
2025-11-04 09:35:15 -0500

    repositorio de certificacion

---
+++ operadores.py
@@ -0,0 +1,14 @@
+#< menor que > mayot que <= menor igual >=mayor igual   == igualdad comparar != diferente a que 
+
+
+food = input("What's the best food?: ")
+
+if food == "rice":
+    print("Yes, rice is the best.")
+elif food == "apple":
+    print("Apples aren't my cup of rice.")
+else:
+    print("Never heard of it!")
+
+ 
+ 
